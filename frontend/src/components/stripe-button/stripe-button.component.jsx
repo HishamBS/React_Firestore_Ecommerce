29bb4d9329258a2626f23a5f207b126c46090aff
Hisham Bin Seddeq <prog.hbs@hotmail.com>
2020-12-12 02:08:23 +0300

    user sagas added

---
+++ frontend/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51HuBg2JdLmE7RICW72CXJ71cgoHAQlyruJjNCxvUXX3eFUp59PdZtuG6BbuRVWaJXmivftTPTJ8nAdsYCyKXhR0D00gYe6yWAh";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Succesful!");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
