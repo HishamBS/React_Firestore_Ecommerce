2ae9d73b13eea31ca53b4fb6651c50156cd16490
Hisham Bin Seddeq <prog.hbs@hotmail.com>
2020-12-14 10:40:00 +0300

    added server and stripe route

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -7,8 +8,23 @@ const StripeCheckoutButton = ({ price }) => {
     "pk_test_51HuBg2JdLmE7RICW72CXJ71cgoHAQlyruJjNCxvUXX3eFUp59PdZtuG6BbuRVWaJXmivftTPTJ8nAdsYCyKXhR0D00gYe6yWAh";
 
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Succesful!");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token: token,
+      },
+    })
+      .then((response) => {
+        alert("succesful payment");
+      })
+      .catch((error) => {
+        console.log("Payment Error: ", error);
+        alert(
+          "There was an issue with your payment! Please make sure you use the provided credit card."
+        );
+      });
   };
 
   return (
