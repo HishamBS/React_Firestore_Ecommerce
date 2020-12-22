fee15ab74dc170bb131c0dd21c7231c77bbf69d7
HishamBS <prog.hbs@hotmail.com>
2020-12-22 23:02:35 +0300

    optimization

---
+++ client/src/components/error-boundary/error-boundary.component.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  ErrorImageOverlay,
+  ErrorImageContainer,
+  ErrorImageText,
+  ErrorImageHeader,
+} from "./error-boundary.styles";
+class ErrorBoundary extends React.Component {
+  state = {
+    hasErrored: false,
+  };
+  static getDerivedStateFromError(error) {
+    return { hasErrored: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.log(error);
+  }
+  render() {
+    if (this.state.hasErrored) {
+      return (
+        <ErrorImageOverlay>
+          <ErrorImageContainer imageUrl="https://i.imgur.com/A040Lxr.png" />
+          <ErrorImageHeader>This Page is Lost in Space</ErrorImageHeader>
+          <ErrorImageText>
+            You thought this mission to the moon would be a quick six month
+            thing. Your neighbor offered to look after your dog. Your high
+            school math teacher was impressed. He once said you wouldn’t amount
+            to anything.You sure showed him. But now here you are, fifty feet
+            from your spaceship with no way to get back. Your dog will be so
+            sad. Your math teacher will be so smug. Pretty devastating.
+          </ErrorImageText>
+        </ErrorImageOverlay>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
